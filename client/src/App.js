537948bf3382ab072036d3fb411cb169a1de6318
Athipat <athipaty@gmail.com>
2025-06-04 19:37:42 +0700

    reset page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-  import {Test} from "./Test.js";
   function App() {
     return (
-      <div>
-        <h1>Hello World!</h1>
+      <div className="p-8">
+        <h1 className="text-2xl text-center font-bold mb-3">Test app!</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores temporibus, nostrum ut modi odio assumenda totam accusamus corporis voluptas a earum commodi odit autem maxime vitae aspernatur eligendi iste quae!</p>
       </div>
     );
   }
