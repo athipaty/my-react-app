dac431382cfa4bbbf01e40953e8585ffb2ffa19e
Athipat <athipaty@gmail.com>
2025-06-11 22:38:27 +0700

    shopping cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,67 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
 
 export default function App() {
+    const products = [
+        { id: 1, name: "Tooth Brush", price: 30 },
+        { id: 2, name: "Tooth Plate", price: 45 },
+        { id: 3, name: "Soap", price: 25 }
+    ];
 
-    const [second, setSecond] = useState(0);
-    const [isRunning, setIsRunning] = useState(false)
+    const [cart, setCart] = useState([]);
 
-    function toggleRunning() {
-        setIsRunning(!isRunning)
+    function handleCart(product) {
+        setCart([...cart, product])
     }
 
-    function handleReset() {
-        setSecond(0);
-        setIsRunning(false)
+    function handleDelete(index1) {
+        const newUpdate = cart.filter((item, index2) => index1 !== index2)
+        setCart(newUpdate)
     }
-
-    function formatTime(totalSeconds) {
-        const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, "0");
-        const minutes = String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, "0");
-        const seconds = String(totalSeconds % 60).padStart(2, "0");
-
-        return `${hours}:${minutes}:${seconds}`;
-    }
-
-    useEffect(() => {
-        let internalId;
-        if (isRunning) {
-            internalId = setInterval(() => {
-                setSecond((previous) => previous + 1)
-            }, 1000)
-        }
-
-        return () => clearInterval(internalId)
-    }, [isRunning])
 
     return (
-        <div className="text-center">
-            <h1 className="text-xl font-bold p-4 mb-2">Stop Watch</h1>
-            <p className="mb-4 text-xl">{formatTime(second)}</p>
+        <div className="text-center p-6">
+            <h1 className="text-2xl font-bold mb-4">Shopping Cart</h1>
+            {products.map((item) => (
+                <div key={item.id} className="border p-3 mb-2 flex justify-between items-center max-w-md mx-auto shadow">
 
-            <button
-                className={`py-2 text-white px-4 rounded ${isRunning === true ? "bg-red-500" : "bg-blue-500"} transition-1s`}
-                onClick={toggleRunning}
-            >{isRunning === true ? "Pause" : "Start"}</button>
+                    <div>
+                        <p className="text-start">{item.name}</p>
+                        <p className="text-gray-500 text-start">{item.price} Baht</p>
+                    </div>
 
-            <button
-                className="bg-green-500 text-white py-2 px-4 rounded ml-2"
-                onClick={handleReset}
+                    <button className="bg-blue-500 text-white px-3 py-1 rounded transition-transform duration-30 active:scale-95 focus:ouline-none shadow" onClick={() => handleCart(item)}>
+                        Add
+                    </button>
 
-            >Reset</button>
+                </div>
+            ))}
+
+            {cart.length > 0 && (
+                <p className="mt-4 font-bold">
+                    รวมทั้งหมด: {cart.reduce((sum, item) => sum + item.price, 0)} บาท
+                </p>
+            )}
+
+            <div className="mt-8">
+                <h2 className="text-xl font-semibold mb-2">🛒 ตะกร้าของคุณ</h2>
+
+                {cart.length === 0 ? (
+                    <p className="text-gray-500">ยังไม่มีสินค้าในตะกร้า</p>
+                ) : (
+                    <ul className="max-w-md mx-auto text-left p-2">
+                        {cart.map((item, index) => (
+                            <li key={index} className="border-b py-2 flex justify-between">
+                                <span>{item.name}</span>
+                                <span>{item.price} ฿</span>
+                                <button className="px-2 py-1 bg-gray-500 text-white rounded" onClick={() => handleDelete(index)}>Delete</button>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+
+
 
         </div>
     )
-};
+}
