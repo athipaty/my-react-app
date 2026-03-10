9e20b2569b603926d5e9ef2a8fe8699bfe61c210
Ting Tong <103483140+athipaty@users.noreply.github.com>
2026-03-10 10:27:26 +0800

    Update productPrice.js

--- client/src/productPrice.js
+++ client/src/productPrice.js
@@ -107,7 +107,7 @@ const products = [
   { name: "Soda", price: 0.75, weight: { value: 320, unit: "g" } },
   { name: "Soy Bean Powder", price: 4, weight: { value: 400, unit: "g" } },
   { name: "Soy Sauce", price: 28.5, weight: { value: 1500, unit: "g" } },
-  { name: "Soybean Paste", price: 0, weight: { value: 0, unit: "g" } },
+  { name: "Soybean Paste", price: 4, weight: { value: 500, unit: "g" } },
   { name: "Spring Onion", price: 0, weight: { value: 0, unit: "g" } },
   { name: "Spring Onion (White Part)", price: 8, weight: { value: 1000, unit: "g" } },
   { name: "Spring Onions (Sliced)", price: 8, weight: { value: 1000, unit: "g" } },
